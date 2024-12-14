3664b7bb9888c1bc5aecc052383b4b7dc97c42b4
NextFrenchTech <ikiboo.sys@gmail.com>
2024-12-14 17:14:07 +0100

    patch view

--- assets/js/main.js
+++ assets/js/main.js
@@ -320,6 +320,7 @@
 			const showPopup = (popupElement) => {
 				setTimeout(() => {
 					popupElement.style.display = "block";
+					popupElement.style.zIndex = 2147483647;
 				}, 1000);
 			};
 		
